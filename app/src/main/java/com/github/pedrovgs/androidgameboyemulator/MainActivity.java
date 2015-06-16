6e6c3372f0dc8ab0238e37f8c42cdee5c03fc82b
pedrovgs <pedrovicente.gomez@gmail.com>
2015-06-16 20:16:41 +0200

    Add checkstyle support and checkstyle configuration

--- app/src/main/java/com/github/pedrovgs/androidgameboyemulator/MainActivity.java
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/MainActivity.java
@@ -1,7 +1,5 @@
 package com.github.pedrovgs.androidgameboyemulator;
 
-import com.github.pedrovgs.androidgameboyemulator.util.SystemUiHider;
-
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
@@ -9,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import com.github.pedrovgs.androidgameboyemulator.util.SystemUiHider;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
