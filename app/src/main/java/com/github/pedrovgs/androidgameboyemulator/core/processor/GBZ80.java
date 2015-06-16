0184ef476a1a9c8cc66ed5e25502c5d67258ce51
pedrovgs <pedrovicente.gomez@gmail.com>
2015-06-16 21:32:45 +0200

    Add registers to the CPU and accessor methods

--- app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/GBZ80.java
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/GBZ80.java
@@ -8,6 +8,14 @@ public class GBZ80 {
   private int programCounter;
   private int stackPointer;
 
+  private int registerA;
+  private int registerB;
+  private int registerC;
+  private int registerE;
+  private int registerH;
+  private int registerL;
+  private int registerF;
+
   private int lastInstructionClockM;
   private int lastInstructionClockT;
 
@@ -30,12 +38,68 @@ public class GBZ80 {
   public int getProgramCounter() {
     return programCounter;
   }
-
-  public int getLastInstructionClockM() {
-    return lastInstructionClockM;
+  
+  public int getRegisterA() {
+    return registerA;
   }
 
-  public int getLastInstructionClockT() {
-    return lastInstructionClockT;
+  public void setRegisterA(int registerA) {
+    this.registerA = registerA;
+  }
+
+  public int getRegisterB() {
+    return registerB;
+  }
+
+  public void setRegisterB(int registerB) {
+    this.registerB = registerB;
+  }
+
+  public int getRegisterC() {
+    return registerC;
+  }
+
+  public void setRegisterC(int registerC) {
+    this.registerC = registerC;
+  }
+
+  public int getRegisterE() {
+    return registerE;
+  }
+
+  public void setRegisterE(int registerE) {
+    this.registerE = registerE;
+  }
+
+  public int getRegisterH() {
+    return registerH;
+  }
+
+  public void setRegisterH(int registerH) {
+    this.registerH = registerH;
+  }
+
+  public int getRegisterL() {
+    return registerL;
+  }
+
+  public void setRegisterL(int registerL) {
+    this.registerL = registerL;
+  }
+
+  public int getRegisterF() {
+    return registerF;
+  }
+
+  public void setRegisterF(int registerF) {
+    this.registerF = registerF;
+  }
+
+  public void setLastInstructionClockM(int lastInstructionClockM) {
+    this.lastInstructionClockM = lastInstructionClockM;
+  }
+
+  public void setLastInstructionClockT(int lastInstructionClockT) {
+    this.lastInstructionClockT = lastInstructionClockT;
   }
 }
