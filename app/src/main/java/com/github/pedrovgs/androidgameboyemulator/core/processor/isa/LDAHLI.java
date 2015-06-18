d502a1bc8dd7b6e77afd21bc6cef18278f96dfb6
pedrovgs <pedrovicente.gomez@gmail.com>
2015-06-18 20:55:57 +0200

    Add new operation to the isa

--- app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/LDAHLI.java
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/LDAHLI.java
@@ -3,21 +3,20 @@ package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
 import com.github.pedrovgs.androidgameboyemulator.core.mmu.MMU;
 import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
 
-public class LDHLIA extends Instruction {
+public class LDAHLI extends Instruction {
 
-  LDHLIA(GBZ80 z80, MMU mmu) {
+  LDAHLI(GBZ80 z80, MMU mmu) {
     super(z80, mmu);
   }
 
   @Override public void execute() {
     int address = (z80.getRegisterH() << 8) + z80.getRegisterL();
-    int value = z80.getRegisterA();
-    mmu.writeByte(address, value);
-    int registerValue = (z80.getRegisterL() + 1) & 255;
+    int memoryValue = mmu.readByte(address);
+    z80.setRegisterA(memoryValue);
+    int registerValue = (z80.getRegisterL() & 255) + 1;
     z80.setRegisterL(registerValue);
     if (z80.getRegisterL() == 0) {
-      int registerH = (z80.getRegisterH() + 1) & 255;
-      z80.setRegisterH(registerH);
+      int registerHValue = (z80.getRegisterH() + 1) & 255;
     }
     z80.setLastInstructionExecutionTime(2);
   }
