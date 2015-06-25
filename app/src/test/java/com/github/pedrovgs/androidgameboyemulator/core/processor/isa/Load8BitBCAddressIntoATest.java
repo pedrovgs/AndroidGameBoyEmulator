a7cc03b40d43f68357ac95ff5c6552ee4f4178e8
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-06-25 19:05:08 +0200

    Add tests to the last instruction added

---
+++ app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Load8BitBCAddressIntoATest.java
@@ -0,0 +1,29 @@
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.InstructionTest;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
+
+public class Load8BitBCAddressIntoATest extends InstructionTest {
+
+  @Test public void shouldLoadTheByteOfMemoryPointedByBCRegisterIntoTheRegisterA() {
+    when(mmu.readByte(anyInt())).thenReturn(ANY_MEMORY_BYTE_VALUE);
+    Instruction instruction = new Load8BitBCAddressIntoA(z80, mmu);
+
+    instruction.execute();
+
+    assertEquals(ANY_MEMORY_BYTE_VALUE, z80.get8BitRegisterValue(Register.A));
+  }
+
+  @Test public void shouldUseTwoCyclesAsLastExecutionTime() {
+    Instruction instruction = new Load8BitBCAddressIntoA(z80, mmu);
+
+    instruction.execute();
+
+    assertEquals(2, z80.getLastInstructionExecutionTime());
+  }
+}
