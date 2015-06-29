d502f3958c63ebd6eea369d99358d559bd55c1c1
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-06-29 23:07:45 +0200

    Improve tests for Load8BitHLAddressInto8BitRegisterTest

--- app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Load8BitHLAddressInto8BitRegisterTest.java
+++ app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Load8BitHLAddressInto8BitRegisterTest.java
@@ -1,10 +1,10 @@
 package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
 
 import com.github.pedrovgs.androidgameboyemulator.InstructionTest;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.when;
 
 public class Load8BitHLAddressInto8BitRegisterTest extends InstructionTest {
@@ -18,7 +18,8 @@ public class Load8BitHLAddressInto8BitRegisterTest extends InstructionTest {
   }
 
   @Test public void shouldStoreMemoryValuePointerByHLRegisterIntoTheDestinyRegister() {
-    when(mmu.readByte(anyInt())).thenReturn(ANY_MEMORY_BYTE_VALUE);
+    z80.set16BitRegisterValue(Register.HL, ANY_16BIT_REGISTER_VALUE);
+    when(mmu.readByte(ANY_16BIT_REGISTER_VALUE)).thenReturn(ANY_MEMORY_BYTE_VALUE);
     Instruction instruction =
         new Load8BitHLAddressInto8BitRegsiter(z80, mmu, ANY_8BIT_DESTINY_REGISTER);
 
