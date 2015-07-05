ce50d43db8a57d91380ab99b1b2ba27f029a7936
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-07-05 16:46:38 +0200

    Add tests of the new instruction

---
+++ app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Load16BitAddressImmPCIntoATest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.InstructionTest;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class Load16BitAddressImmPCIntoATest extends InstructionTest {
+
+  @Test public void shouldUseFourCyclesAsLastInstructionExecutionTime() {
+    Instruction instruction = new Load16BitImmPCIntoA(z80, mmu);
+
+    instruction.execute();
+
+    assertEquals(4, z80.getLastInstructionExecutionTime());
+  }
+
+  @Test public void shouldIncrementProgramCounter() {
+    z80.setProgramCounter(ANY_16BIT_REGISTER_VALUE);
+    Instruction instruction = new Load16BitImmPCIntoA(z80, mmu);
+
+    instruction.execute();
+
+    assertEquals(ANY_16BIT_REGISTER_VALUE + 1, z80.getProgramCounter());
+  }
+
+  @Test public void shouldLoad16BitPortAddressPointedByTheProgramCounterIntoTheRegisterA() {
+    z80.setProgramCounter(ANY_16BIT_REGISTER_VALUE);
+    when(mmu.readByte(ANY_16BIT_REGISTER_VALUE + 1)).thenReturn(ANY_MEMORY_BYTE_VALUE);
+    Instruction instruction = new Load16BitImmPCIntoA(z80, mmu);
+
+    instruction.execute();
+
+    assertEquals(ANY_MEMORY_BYTE_VALUE, z80.get8BitRegisterValue(Register.A));
+  }
+}
