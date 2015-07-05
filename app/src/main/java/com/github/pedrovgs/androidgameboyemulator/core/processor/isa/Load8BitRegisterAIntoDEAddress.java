3d4249b1801755f3b4c2a140b6c2f5209357a34b
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-07-05 17:29:18 +0200

    Add another instruction to the isa

---
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Load8BitRegisterAIntoDEAddress.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.core.mmu.MMU;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
+
+public class Load8BitRegisterAIntoDEAddress extends Instruction {
+
+  Load8BitRegisterAIntoDEAddress(GBZ80 z80, MMU mmu) {
+    super(z80, mmu);
+  }
+
+  @Override public void execute() {
+    byte registerValue = z80.get8BitRegisterValue(Register.A);
+    int address = z80.get16BitRegisterValue(Register.DE);
+    mmu.writeByte(address, registerValue);
+    z80.setLastInstructionExecutionTime(2);
+  }
+}
