4121e6e76d69f8cf6bcf3eccbbe830774da0adff
pedrovgs <pedrovicente.gomez@gmail.com>
2015-07-19 19:01:58 +0200

    Add tests to the last instruction added to the project

---
+++ app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Sbc8BitAddressHLToAIntoATest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.InstructionTest;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class Sbc8BitAddressHLToAIntoATest extends InstructionTest {
+
+  @Test public void shouldUseOneCycleAsLastInstructionExecutionTime() {
+    Instruction instruction = new Sbc8BitAddressHLToAIntoA(z80, mmu);
+
+    instruction.execute();
+
+    assertEquals(2, z80.getLastInstructionExecutionTime());
+  }
+
+  @Test public void shouldSubtractTheMemoryValuePointedByHLPlusCYFlagPlusAAndStoreTheResultIntoA() {
+    z80.enableFlagCY();
+    z80.set16BitRegisterValue(Register.HL, ANY_16BIT_REGISTER_VALUE);
+    when(mmu.readByte(ANY_16BIT_REGISTER_VALUE)).thenReturn((byte) 1);
+    z80.set8BitRegisterValue(Register.A, (byte) 3);
+    Instruction instruction = new Sbc8BitAddressHLToAIntoA(z80, mmu);
+
+    instruction.execute();
+
+    assertEquals(1, z80.get8BitRegisterValue(Register.A));
+  }
+}
