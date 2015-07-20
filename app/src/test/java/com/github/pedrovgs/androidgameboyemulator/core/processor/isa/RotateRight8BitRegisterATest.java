845a3acf82d8488b38cea4d51ecbe05280ebdc42
pedrovgs <pedrovicente.gomez@gmail.com>
2015-07-20 22:14:33 +0200

    Add tests to the last instruction added

---
+++ app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/RotateRight8BitRegisterATest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.InstructionTest;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class RotateRight8BitRegisterATest extends InstructionTest {
+
+  @Test public void shouldUseOneCycleAsLastInstructionExecutionTime() {
+    Instruction instruction = new RotateRight8BitRegisterA(z80);
+
+    instruction.execute();
+
+    assertEquals(1, z80.getLastInstructionExecutionTime());
+  }
+
+  @Test public void shouldDisableAllFlagsExceptTheCYOne() {
+    Instruction instruction = new RotateRight8BitRegisterA(z80);
+
+    instruction.execute();
+
+    assertFalse(z80.isFlagHEnabled());
+    assertFalse(z80.isFlagNEnabled());
+    assertFalse(z80.isFlagZEnabled());
+  }
+
+  @Test public void shouldRotateRegisterAToTheRightWithoutUseTheCarryFlag() {
+    z80.set8BitRegisterValue(Register.A, (byte) 7);
+    Instruction instruction = new RotateRight8BitRegisterA(z80);
+
+    instruction.execute();
+
+    assertEquals((byte) 3, z80.get8BitRegisterValue(Register.A));
+  }
+
+  @Test public void shouldRotateRegisterAToTheRightUsingTheCarryFlag() {
+    z80.enableFlagCY();
+    z80.set8BitRegisterValue(Register.A, (byte) 7);
+    Instruction instruction = new RotateRight8BitRegisterA(z80);
+
+    instruction.execute();
+
+    assertEquals((byte) 0x83, z80.get8BitRegisterValue(Register.A));
+  }
+}
