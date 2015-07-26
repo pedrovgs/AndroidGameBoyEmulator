f28cfba35e1fa348268b7ffb25e141bc09c7e6c0
pedrovgs <pedrovicente.gomez@gmail.com>
2015-07-26 19:23:30 +0200

    Add another instruction to the isa

---
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/SwipeLeft8BitHLAddress.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.core.mmu.MMU;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
+
+public class SwipeLeft8BitHLAddress extends SwipeLeft8Bit {
+
+  public SwipeLeft8BitHLAddress(GBZ80 z80, MMU mmu) {
+    super(z80, mmu);
+  }
+
+  @Override protected byte getValue() {
+    int address = z80.get16BitRegisterValue(Register.HL);
+    return mmu.readByte(address);
+  }
+
+  @Override protected void storeValue(byte value) {
+    int address = z80.get16BitRegisterValue(Register.HL);
+    mmu.writeByte(address, value);
+  }
+
+  @Override protected void setLastInstructionExecutionTime() {
+    z80.setLastInstructionExecutionTime(4);
+  }
+}
