66ecdc9aaff1239270a49e91e0a5b557d1ae9ed0
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-07-27 08:10:49 +0200

    Add instruction to the isa

---
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Rst.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
+
+public class Rst extends Instruction {
+
+  private final int pc;
+
+  Rst(GBZ80 z80, int pc) {
+    super(z80);
+    this.pc = pc;
+  }
+
+  @Override public void execute() {
+    int programCounter = z80.getProgramCounter();
+    pushTwice(programCounter);
+    z80.setProgramCounter(pc);
+  }
+}
