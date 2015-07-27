06141e44afc71a3354cd277f91fe96ca5774e62d
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-07-27 07:33:00 +0200

    Add tests to the previously implemented instruction

---
+++ app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Set8BitRegisterTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.InstructionTest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Set8BitRegisterTest extends InstructionTest {
+
+  @Test public void shouldUseTwoCyclesAsLastInstructionExecutionTime() {
+    Instruction instruction =
+        new Set8BitRegister(z80, mmu, ANY_BIT_VALUE, ANY_8BIT_SOURCE_REGISTER);
+
+    instruction.execute();
+
+    assertEquals(2, z80.getLastInstructionExecutionTime());
+  }
+
+  @Test public void shouldSetToOneTheBitIndicatedInTheRegisterPassedAsArgument() {
+    Instruction instruction = new Set8BitRegister(z80, mmu, 1, ANY_8BIT_SOURCE_REGISTER);
+
+    instruction.execute();
+
+    assertEquals(2, z80.get8BitRegisterValue(ANY_8BIT_SOURCE_REGISTER));
+  }
+}
