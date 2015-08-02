599ed6054bb66b361254b191ae6ad5cbef54ad8a
pedrovgs <pedrovicente.gomez@gmail.com>
2015-08-02 21:15:45 +0200

    Add another instruction to the isa

---
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/CPL.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
+
+class CPL extends Instruction {
+  CPL(GBZ80 z80) {
+    super(z80);
+  }
+
+  @Override public void execute() {
+    z80.enableFlagN();
+    z80.enableFlagH();
+    byte registerAValue = z80.get8BitRegisterValue(Register.A);
+    z80.set8BitRegisterValue(Register.A, (byte) ~registerAValue);
+    setLastExecutionTime(1);
+  }
+}
