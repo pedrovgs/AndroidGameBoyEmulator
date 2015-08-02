9596b468d2cc8951bd880afc15df3a3602f8e756
pedrovgs <pedrovicente.gomez@gmail.com>
2015-08-02 20:57:19 +0200

    Add another instruction to the isa

---
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Dec16BitStackPointer.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015 Pedro Vicente Gomez Sanchez.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
+
+class Dec16BitStackPointer extends Instruction {
+  Dec16BitStackPointer(GBZ80 z80) {
+    super(z80);
+  }
+
+  @Override public void execute() {
+    int value = z80.getStackPointer();
+    z80.setStackPointer(value - 1);
+    z80.setLastInstructionExecutionTime(2);
+  }
+}
