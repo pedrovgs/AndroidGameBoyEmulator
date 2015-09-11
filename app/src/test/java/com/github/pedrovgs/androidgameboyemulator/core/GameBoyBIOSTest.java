33dcc894ff47ab0eac2e523d089b39dba7aefae0
pedrovgs <pedrovicente.gomez@gmail.com>
2015-09-11 19:09:04 +0200

    Improve tests to be sure the first bios stage is working. Now we are ready to continue debugging the game boy bios execution manually

--- app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/GameBoyBIOSTest.java
+++ app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/GameBoyBIOSTest.java
@@ -5,6 +5,7 @@ import com.github.pedrovgs.androidgameboyemulator.core.gameloader.GameLoader;
 import com.github.pedrovgs.androidgameboyemulator.core.gpu.GPU;
 import com.github.pedrovgs.androidgameboyemulator.core.mmu.MMU;
 import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
 import java.io.IOException;
 import org.junit.Test;
 
@@ -13,9 +14,8 @@ import static org.junit.Assert.assertFalse;
 
 public class GameBoyBIOSTest {
 
-  private static final int INITIALIZE_LCD_STAGE_TICKS = 26823;
   private static final int STACK_POINTER_INITIAL_VALUE = 0xFFFE;
-  private static final int FIRST_STAGE_FINISH_PROGRAM_COUNTER = 8;
+  private static final int FIRST_STAGE_FINISH_PROGRAM_COUNTER = 12;
   private static final int INITIALIZE_AUDIO_STAGE_TICKS = 11;
 
   private GBZ80 z80;
@@ -32,13 +32,22 @@ public class GameBoyBIOSTest {
 
   @Test public void shouldInitializeVideoMemoryWithZero() throws IOException {
     GameBoy gameBoy = givenAGameBoy();
+    fillVRAMWithNoise();
 
     tickUntilFirstBiosStageFinished(gameBoy);
 
     assertVideoMemoryIsInitializedToZero();
   }
 
-  @Test public void shouldFinishFirstStageWithTheNExtStageProgramCounter() throws IOException {
+  @Test public void shouldFinisBIOSFirstStageWithAConcreteHLValue() throws IOException {
+    GameBoy gameBoy = givenAGameBoy();
+
+    tickUntilFirstBiosStageFinished(gameBoy);
+
+    assertEquals(0X8000 - 1, z80.get16BitRegisterValue(Register.HL));
+  }
+
+  @Test public void shouldFinishFirstStageWithTheNextStageProgramCounter() throws IOException {
     GameBoy gameBoy = givenAGameBoy();
 
     tickUntilFirstBiosStageFinished(gameBoy);
@@ -78,7 +87,9 @@ public class GameBoyBIOSTest {
   }
 
   private void tickUntilFirstBiosStageFinished(GameBoy gameBoy) throws IOException {
-    tickGameBoy(gameBoy, INITIALIZE_LCD_STAGE_TICKS);
+    while (z80.getProgramCounter() <= 0x0A) {
+      gameBoy.tick();
+    }
   }
 
   private void tickUntilSecondBiosStageFinished(GameBoy gameBoy) throws IOException {
@@ -86,8 +97,14 @@ public class GameBoyBIOSTest {
     tickGameBoy(gameBoy, INITIALIZE_AUDIO_STAGE_TICKS);
   }
 
-  private void assertVideoMemoryIsInitializedToZero() {
+  private void fillVRAMWithNoise() {
     for (int address = 0x8000; address < 0x9FFF; address++) {
+      mmu.writeByte(address, (byte) 1);
+    }
+  }
+
+  private void assertVideoMemoryIsInitializedToZero() {
+    for (int address = 0x9FFF; address >= 0x8000; address--) {
       byte value = mmu.readByte(address);
       assertEquals("Address = " + address + " not initialized to 0", 0, value);
     }
