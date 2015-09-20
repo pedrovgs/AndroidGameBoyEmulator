822f3e6f5ffe875b975ae3984bbbef70f127f32d
pedrovgs <pedrovicente.gomez@gmail.com>
2015-09-20 18:42:54 +0200

    Add the new instruction to the project

--- app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Load8BitRegisterIntoHLAddress.java
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Load8BitRegisterIntoHLAddress.java
@@ -1,7 +1,22 @@
 package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
 
-/**
- * @author Pedro Vicente Gómez Sánchez.
- */
-public class Load8BitRegisterIntoHLAddress {
+import com.github.pedrovgs.androidgameboyemulator.core.mmu.MMU;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
+import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
+
+class Load8BitRegisterIntoHLAddress extends Instruction {
+
+  private final Register sourceRegister;
+
+  Load8BitRegisterIntoHLAddress(GBZ80 z80, MMU mmu, Register sourceRegister) {
+    super(z80, mmu);
+    this.sourceRegister = sourceRegister;
+  }
+
+  @Override public void execute() {
+    byte registerValue = z80.get8BitRegisterValue(sourceRegister);
+    int address = z80.get16BitRegisterValue(Register.HL);
+    mmu.writeByte(address, registerValue);
+    setLastExecutionTime(2);
+  }
 }
