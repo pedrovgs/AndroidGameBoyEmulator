1654f39330de36a3f16f848893b9f619f875bbed
pedrovgs <pedrovicente.gomez@gmail.com>
2015-09-21 11:27:50 +0200

    Improve Load16BitImmPcIntoStackPointer

--- app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Load16BitImmPcIntoStackPointer.java
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Load16BitImmPcIntoStackPointer.java
@@ -20,7 +20,7 @@ package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
 import com.github.pedrovgs.androidgameboyemulator.core.mmu.MMU;
 import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
 
-public class Load16BitImmPcIntoStackPointer extends Instruction {
+class Load16BitImmPcIntoStackPointer extends Instruction {
 
   Load16BitImmPcIntoStackPointer(GBZ80 z80, MMU mmu) {
     super(z80, mmu);
---
+++ app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Load16BitImmPcIntoStackPointerTest.java
@@ -0,0 +1,28 @@
+package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
+
+import com.github.pedrovgs.androidgameboyemulator.InstructionTest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class Load16BitImmPcIntoStackPointerTest extends InstructionTest {
+
+  @Test public void shouldUseThreeCyclesAsLastInstructionExecutionTime() {
+    Instruction instruction = new Load16BitImmPcIntoStackPointer(z80, mmu);
+
+    instruction.execute();
+
+    assertEquals(3, z80.getLastInstructionExecutionTime());
+  }
+
+  @Test public void shouldLoadTheContentOfTheMemoryPointedByPCIntoTheStackPointer() {
+    z80.setProgramCounter(ANY_PROGRAM_COUNTER);
+    when(mmu.readWord(ANY_PROGRAM_COUNTER)).thenReturn(ANY_16BIT_REGISTER_VALUE);
+    Instruction instruction = new Load16BitImmPcIntoStackPointer(z80, mmu);
+
+    instruction.execute();
+
+    assertEquals(ANY_16BIT_REGISTER_VALUE, z80.getStackPointer());
+  }
+}
