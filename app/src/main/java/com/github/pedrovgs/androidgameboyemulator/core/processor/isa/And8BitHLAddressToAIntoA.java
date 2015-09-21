20a73adf86e4a4e6e46d40c8763c3ec3e62a51a3
pedrovgs <pedrovicente.gomez@gmail.com>
2015-09-21 12:27:24 +0200

    Improve And8BitHLAddressToAIntoA

--- app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/And8BitHLAddressToAIntoA.java
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/And8BitHLAddressToAIntoA.java
@@ -21,7 +21,7 @@ import com.github.pedrovgs.androidgameboyemulator.core.mmu.MMU;
 import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
 import com.github.pedrovgs.androidgameboyemulator.core.processor.Register;
 
-public class And8BitHLAddressToAIntoA extends And8Bit {
+class And8BitHLAddressToAIntoA extends And8Bit {
 
   And8BitHLAddressToAIntoA(GBZ80 z80, MMU mmu) {
     super(z80, mmu);
