291d79e90f0bbb474adbaf3006eb649fbe862122
pedrovgs <pedrovicente.gomez@gmail.com>
2015-09-21 12:37:36 +0200

    Improve Cp8BitImmPCWithA

--- app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Cp8BitImmPCWithA.java
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/processor/isa/Cp8BitImmPCWithA.java
@@ -20,7 +20,7 @@ package com.github.pedrovgs.androidgameboyemulator.core.processor.isa;
 import com.github.pedrovgs.androidgameboyemulator.core.mmu.MMU;
 import com.github.pedrovgs.androidgameboyemulator.core.processor.GBZ80;
 
-public class Cp8BitImmPCWithA extends Cp8Bit {
+class Cp8BitImmPCWithA extends Cp8Bit {
 
   Cp8BitImmPCWithA(GBZ80 z80, MMU mmu) {
     super(z80, mmu);
