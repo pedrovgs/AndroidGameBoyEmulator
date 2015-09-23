b15ee64b3794aed50efa0dfc268929eeb35c5632
pedrovgs <pedrovicente.gomez@gmail.com>
2015-09-23 11:31:11 +0200

    Change bios test to find the tick where the game boy starts failing

--- app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/GameBoyBIOSTest.java
+++ app/src/test/java/com/github/pedrovgs/androidgameboyemulator/core/GameBoyBIOSTest.java
@@ -181,8 +181,8 @@ public class GameBoyBIOSTest {
 
     tickUntilBIOSLoaded(gameBoy);
 
-    //for (int i = 0; i < 16030; i++) {
-    for (int i = 0; i < 17030; i++) {
+    for (int i = 0; i < 16010; i++) {
+      //for (int i = 0; i < 37030; i++) {
       gameBoy.tick();
       System.out.println(z80.getProgramCounter());
       System.out.println(gpu.getCurrentLine());
