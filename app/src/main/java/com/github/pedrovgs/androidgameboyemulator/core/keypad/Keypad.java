6186e5208c7362c70dd8830f3b8330dea2906662
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-10-12 11:16:02 +0200

    Remove keypad column update from the Keypad.java class

--- app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/keypad/Keypad.java
+++ app/src/main/java/com/github/pedrovgs/androidgameboyemulator/core/keypad/Keypad.java
@@ -24,10 +24,7 @@ import java.util.Map;
 public class Keypad {
 
   private static final int KEYPAD_ADDRESS = 0xFF00;
-  private static final int FIRST_COLUMN = 0x10;
-  private static final int SECOND_COLUMN = 0x20;
 
-  private final boolean[] pressedKeys = new boolean[8];
   private final Map<Key, Byte> keyDownValues = new HashMap<Key, Byte>();
   private final Map<Key, Byte> keyUpValues = new HashMap<Key, Byte>();
 
@@ -39,19 +36,15 @@ public class Keypad {
   }
 
   public void keyUp(Key key) {
-    pressedKeys[key.ordinal()] = false;
     byte keyUpValue = getKeypadUpValue(key);
     byte newKeypadValue = (byte) (mmu.readByte(KEYPAD_ADDRESS) & 0xFF | keyUpValue);
     mmu.writeByte(KEYPAD_ADDRESS, newKeypadValue);
-    updateColumn();
   }
 
   public void keyDown(Key key) {
-    pressedKeys[key.ordinal()] = true;
     byte keyDownValue = getKeyDownValue(key);
     byte newKeypadValue = (byte) (mmu.readByte(KEYPAD_ADDRESS) & 0xFF & keyDownValue);
     mmu.writeByte(KEYPAD_ADDRESS, newKeypadValue);
-    updateColumn();
   }
 
   private byte getKeyDownValue(Key key) {
@@ -87,31 +80,5 @@ public class Keypad {
     keyUpValues.put(Key.B, (byte) 0x2);
     keyUpValues.put(Key.SELECT, (byte) 0x4);
     keyUpValues.put(Key.START, (byte) 0x8);
-  }
-
-  private void updateColumn() {
-    byte newKeyValue = (byte) (mmu.readByte(KEYPAD_ADDRESS) & 0xFF);
-    if (isFirstColumnEnabled()) {
-      newKeyValue &= ~FIRST_COLUMN;
-    } else {
-      newKeyValue |= FIRST_COLUMN;
-    }
-
-    if (isSecondColumnEnabled()) {
-      newKeyValue &= ~SECOND_COLUMN;
-    } else {
-      newKeyValue |= SECOND_COLUMN;
-    }
-    mmu.writeByte(KEYPAD_ADDRESS, newKeyValue);
-  }
-
-  private boolean isFirstColumnEnabled() {
-    return pressedKeys[Key.START.ordinal()] || pressedKeys[Key.SELECT.ordinal()]
-        || pressedKeys[Key.B.ordinal()] || pressedKeys[Key.A.ordinal()];
-  }
-
-  private boolean isSecondColumnEnabled() {
-    return pressedKeys[Key.DOWN.ordinal()] || pressedKeys[Key.UP.ordinal()]
-        || pressedKeys[Key.LEFT.ordinal()] || pressedKeys[Key.RIGHT.ordinal()];
   }
 }
